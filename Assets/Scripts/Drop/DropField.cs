f764ff0166970dd4d6ee68661bb49211a4ed18c8
Alex <playstel.studio@gmail.com>
2022-09-23 10:46:52 +0300

    Unity Version Update

--- Assets/Scripts/Drop/DropField.cs
+++ Assets/Scripts/Drop/DropField.cs
@@ -19,6 +19,7 @@ public class DropField : MonoBehaviour, IDropHandler, IPointerEnterHandler, IPoi
         if (cardDrag)
         {
             cardDrag.DefaultParent = _transform;
+            cardDrag.DefaultTempCardParent = _transform;
         }
     }
 
@@ -30,6 +31,7 @@ public class DropField : MonoBehaviour, IDropHandler, IPointerEnterHandler, IPoi
         
         if (cardDrag)
         {
+            cardDrag.DefaultParent = _transform;
             cardDrag.DefaultTempCardParent = _transform;
         }
     }
